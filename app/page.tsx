058b6deb6242beec2062a347b0c479407cda6938
Farhan <farhanram71@gmail.com>
2024-07-01 00:07:55 +0700

    Update about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import RecentProjects from "@/components/RecentProjects";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5">
       <div className="max-w-7xl w-full">
+        <Header navItems={navItems}/>
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
