05151577dea8848c9ff09a202079a19a50329c71
Farhan <farhanram71@gmail.com>
2024-07-01 00:06:02 +0700

    Add project section

---
+++ data/index.ts
@@ -0,0 +1,118 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#project" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/laptop.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Evolving, learning with every opportunity",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Sharp Mapping Company Profile",
+    des: "Sharp Mapping is a leading company in the field of geographical mapping solutions. Their goal is to provide accurate, innovative, and reliable mapping services to various industries.",
+    img: "/sharp.png",
+    iconLists: ["/lara.svg", "/tail.svg", "/livewire.svg"],
+    link: "https://sharpmapping.com/",
+  },
+  {
+    id: 2,
+    title: "Seqola - School Management System",
+    des: "A comprehensive solution designed to streamline administrative tasks such as student enrollment, attendance tracking, grade management.",
+    img: "/seqola.png",
+    iconLists: ["/lara.svg", "/tail.svg", "/vue.svg"],
+  },
+  {
+    id: 3,
+    title: "Gumilang Jati",
+    des: "Providing comprehensive digital marketing solutions including social media management and engaging video content creation. ",
+    img: "/gumilang.png",
+    iconLists: ["/lara.svg", "/tail.svg", "/livewire.svg"],
+    link: "https://gumilangjati.reviu.cloud/",
+  },
+  {
+    id: 4,
+    title: "Glori Mitra Sukses - Ecommerce Website",
+    des: "Explore and purchase products conveniently, with features including secure payment gateways, product categorization, user reviews, and personalized shopping experiences tailored to meet customer needs.",
+    img: "/gms.png",
+    iconLists: ["/word.svg", "/php.svg", "/woo.svg"],
+    link: "https://glorimitrasukses.co.id/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Sotware Engineer Intern - Berani Digital ID",
+    description:
+      "Contributed significantly to the creation of a dynamic and interactive web-based platform, enhancing user engagement and improving overall functionality.",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer - Adici Jaya Bersama",
+    description:
+      "Played a crucial role in bridging the gap between the design and technical implementation, ensuring a smooth user experience and efficient functionality from concept to deployment.",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Developer",
+    description:
+      "Led the development of a web-based app for a client, from initial concept to deployment on app stores.",
+  },
+  {
+    id: 4,
+    title: "Web Developer Intern - Glori Mitra Sukses",
+    description:
+      "Developed and maintained e-commerce website, ensuring seamless user experience and operational efficiency.",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/hanvee",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://instagram.com/farhanrmdn71",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/farhanram",
+  },
+];
