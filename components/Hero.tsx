adde68a4fe256ae6b47cab7a18f87753c0039e21
Farhan <farhanram71@gmail.com>
2024-07-01 00:07:36 +0700

    Update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         />
         <Spotlight
           className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
+          fill="#3965d4"
         />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
@@ -32,15 +32,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Crafting Digital Experiences with Precision and Passion"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-2 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Farhan Ramadhan, a Web Developer based in Jakarta, Indonesia.
+            Hi! I&apos;m Farhan Ramadhan, a Web Developer based in Jakarta.
           </p>
 
-          <a href="#about">
+          <a href="#project">
             <MagicButton
               title="Show my work"
               icon={<FaArrowRightLong />}
